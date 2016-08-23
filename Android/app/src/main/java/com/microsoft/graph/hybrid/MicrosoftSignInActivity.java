ec2d5631da7ee6d5b03e012671d34f5702e4f221
Ricardo Loo <rckloo@yahoo.com>
2016-08-22 22:01:15 -0700

    Added basic WebView handling the authorization code flow

--- Android/app/src/main/java/com/microsoft/graph/hybrid/MicrosoftSignInActivity.java
+++ Android/app/src/main/java/com/microsoft/graph/hybrid/MicrosoftSignInActivity.java
@@ -1,8 +1,12 @@
 package com.microsoft.graph.hybrid;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MicrosoftSignInActivity extends AppCompatActivity {
   private WebView mAuthWebView;
@@ -13,6 +17,20 @@ public class MicrosoftSignInActivity extends AppCompatActivity {
     setContentView(R.layout.activity_microsoft_sign_in);
 
     mAuthWebView = (WebView)findViewById(R.id.webView);
+    WebSettings webSettings = mAuthWebView.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+
+    mAuthWebView.setWebViewClient(new WebViewClient(){
+      @Override
+      public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+
+        if(url.contains("https://firebasehybrid.azurewebsites.net/microsoft/callback?code")) {
+          Intent backToApp = new Intent(view.getContext(), MainActivity.class);
+          startActivity(backToApp);
+        }
+      }
+    });
 
     mAuthWebView.loadUrl("https://firebasehybrid.azurewebsites.net/microsoft/signin");
   }
